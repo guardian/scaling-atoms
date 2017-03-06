7fc7cdeec36b115f988485a50d385119c5820f92
Philip McMahon <philip.mcmahon@guardian.co.uk>
2017-03-06 11:59:42 +0000

    Empty array rather than false.

--- public/js/reducers/atomListReducer.js
+++ public/js/reducers/atomListReducer.js
@@ -4,7 +4,7 @@ export default function atomList(state = null, action) {
     switch (action.type) {
 
         case 'ATOM_LIST_GET_RECEIVE':
-            return action.atomList || false;
+            return action.atomList || [];
 
         default:
             return state;
