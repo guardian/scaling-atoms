60f79d54018f286ac0cdc184361f45a43bc52cd7
Philip McMahon <philip.mcmahon@guardian.co.uk>
2017-03-15 12:19:53 +0000

    Make header more modular.

--- public/js/components/Header/Header.js
+++ public/js/components/Header/Header.js
@@ -56,62 +56,48 @@ class Header extends React.Component {
   }
 
   renderPublishedState = () => {
-    if(this.isEditor() && this.props.atom) {
-
-      const atomPublishState = publishState(this.props.atom);
-
-      return (
-        <nav className="main-nav" role="navigation">
-          <ul className="main-nav__list">
-            <li className="toolbar__item main-nav__item">
-              <span className={`publish-state publish-state--${atomPublishState.id}`}>{atomPublishState.text}</span>
-            </li>
-            <li className="toolbar__item main-nav__item">
-              {this.renderSaveState()}
-            </li>
-          </ul>
-        </nav>
-      );
-    }
-    return false;
-  }
-
-  renderPublishButton = () => {
-    if(this.isEditor() && this.props.atom) {
-
-      const atomPublishState = publishState(this.props.atom);
-
-      return (
-        <div className="toolbar__container">
-          <div className="toolbar__item">
-            {this.props.presence ? <PresenceIndicator presence={this.props.presence} /> : false}
-          </div>
-          <button disabled={atomPublishState.id === 'published'} type="button" onClick={this.publishAtom} className="toolbar__item toolbar__button">Publish</button>
-          {this.renderTakeDownButton(atomPublishState)}
-        </div>
-      );
-    }
-    return false;
-  }
-
-  renderCreateNewButton = () => {
-    if(this.isList()) {
-      return (
-          <div className="toolbar__container">
-            <Link to="/create" className="toolbar__item toolbar__button">
-            <button type="button" className="">Create new</button>
-              </Link>
-          </div>
-      );
-    }
-    return false;
+    const atomPublishState = publishState(this.props.atom);
+    return (
+        <span className={`publish-state publish-state--${atomPublishState.id}`}>{atomPublishState.text}</span>
+    );
   }
 
   renderTakeDownButton = (atomPublishState) => {
-
-    if(this.isEditor() && atomPublishState.id !== 'draft') {
+    if(atomPublishState.id !== 'draft') {
       return (
         <button type="button" disabled={atomPublishState.id === 'taken-down'} onClick={this.takeDownAtom} className="toolbar__item toolbar__button">Take down</button>
+      );
+    }
+    return false;
+  }
+
+  renderHeaderRight = () => {
+
+    const atomPublishState = publishState(this.props.atom);
+
+    return (
+        <div className="toolbar__container">
+          {this.props.presence ? <PresenceIndicator presence={this.props.presence} /> : false}
+          {this.isEditor() && this.props.atom ? <button disabled={atomPublishState.id === 'published'} type="button" onClick={this.publishAtom} className="toolbar__item toolbar__button">Publish</button> : false}
+          {this.isEditor() && this.props.atom ? this.renderTakeDownButton(atomPublishState) : false}
+          {this.isList() ? <Link to="/create" className="toolbar__item toolbar__button"><button type="button" className="">Create new</button></Link> : false}
+        </div>
+    );
+  }
+
+  renderHeaderLeft = () => {
+    if (this.isEditor() && this.props.atom) {
+      return (
+          <nav className="main-nav" role="navigation">
+            <ul className="main-nav__list">
+              <li className="toolbar__item main-nav__item">
+                {this.renderPublishedState()}
+              </li>
+              <li className="toolbar__item main-nav__item">
+                {this.renderSaveState()}
+              </li>
+            </ul>
+          </nav>
       );
     }
     return false;
@@ -131,10 +117,9 @@ class Header extends React.Component {
                 </div>
               </Link>
             </header>
-            {this.renderPublishedState()}
+            {this.renderHeaderLeft()}
           </div>
-            {this.renderPublishButton()}
-          {this.renderCreateNewButton()}
+            {this.renderHeaderRight()}
         </div>
     );
   }
