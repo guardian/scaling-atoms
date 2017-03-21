53f80dcfb81bf4945ef731a46eba0f188a795ef5
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-03-21 14:34:43 +0000

    initial split

--- public/js/components/Header/Header.js
+++ public/js/components/Header/Header.js
@@ -31,85 +31,8 @@ class Header extends React.Component {
     isFindPage: PropTypes.bool.isRequired
   }
 
-  publishAtom = () => {
-    this.props.atomActions.publishAtom(this.props.atom);
-  }
-
-  takeDownAtom = () => {
-    this.props.atomActions.takeDownAtom(this.props.atom);
-  }
-
-  isEditor = () => {
-    return location.pathname.search(new RegExp('\/atoms\/.*\/.*\/edit', 'g')) >= 0;
-  }
-
-  timeSinceLastModified = () => {
-    if (this.props.atom.contentChangeDetails.created || this.props.atom.contentChangeDetails.lastModified) {
-      const dateNow = Date.now();
-      const lastModified = this.props.atom.contentChangeDetails.lastModified ? this.props.atom.contentChangeDetails.lastModified.date : this.props.atom.contentChangeDetails.created.date;
-      return distanceInWords(dateNow, lastModified, {addSuffix: true});
-    }
-    return false;
-  }
-
-  renderSaveState = () => {
-
-    if(this.props.saveState.saving === saveStateVals.inprogress) {
-      return (
-          <span className="loader save-state__loader">Saving</span>
-      );
-    }
-    return (
-      <span><span className="save-state">Saved</span> {this.timeSinceLastModified()}</span>
-    );
-  }
-
-  renderPublishedState = () => {
-    const atomPublishState = publishState(this.props.atom);
-    return (
-        <span className={`publish-state publish-state--${atomPublishState.id}`}>{atomPublishState.text}</span>
-    );
-  }
-
-  renderTakeDownButton = (atomPublishState) => {
-    if(atomPublishState.id !== 'draft') {
-      return (
-        <button type="button" disabled={atomPublishState.id === 'taken-down'} onClick={this.takeDownAtom} className="toolbar__item toolbar__button">Take down</button>
-      );
-    }
-    return false;
-  }
-
-  renderHeaderRight = () => {
-
-    const atomPublishState = publishState(this.props.atom);
-
-    return (
-        <div className="toolbar__container">
-          {this.isEditor() && this.props.presence ? <PresenceIndicator presence={this.props.presence} /> : false}
-          {this.isEditor() && this.props.atom ? <button disabled={atomPublishState.id === 'published'} type="button" onClick={this.publishAtom} className="toolbar__item toolbar__button">Publish</button> : false}
-          {this.isEditor() && this.props.atom ? this.renderTakeDownButton(atomPublishState) : false}
-          {this.props.isFindPage ? <Link to="/create" className="toolbar__item toolbar__button"><button type="button" className="">Create new</button></Link> : false}
-        </div>
-    );
-  }
-
-  renderAtomStates = () => {
-    if (this.isEditor() && this.props.atom) {
-      return (
-          <nav className="main-nav" role="navigation">
-            <ul className="main-nav__list">
-              <li className="toolbar__item main-nav__item">
-                {this.renderPublishedState()}
-              </li>
-              <li className="toolbar__item main-nav__item">
-                {this.renderSaveState()}
-              </li>
-            </ul>
-          </nav>
-      );
-    }
-    return false;
+  state: {
+    editMode: false
   }
 
   render () {
@@ -131,9 +54,7 @@ class Header extends React.Component {
                 </div>
               </Link>
             </header>
-            {this.renderAtomStates()}
           </div>
-            {this.renderHeaderRight()}
         </div>
     );
   }
