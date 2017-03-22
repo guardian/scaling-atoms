1cab75ae8fab12cfe361ea8f17da931c9bd7cb20
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-03-22 12:20:39 +0000

    browse header styles

--- public/js/components/Header/BrowseHeader.js
+++ public/js/components/Header/BrowseHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export default class BrowseHeader extends React.Component {
 
   render() {
     return (
-      <p>Browse Header</p>
+      <Link to="/create" className="toolbar__button">Create New Atom</Link>
     );
   }
 }
