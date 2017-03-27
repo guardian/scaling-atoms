6332f1ec23bf521befdd8ec58b02b16af8a0b448
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-03-27 17:56:06 +0100

    fix props passing in recipe editor

--- public/js/components/AtomEdit/CustomEditors/RecipeEditor.js
+++ public/js/components/AtomEdit/CustomEditors/RecipeEditor.js
@@ -6,7 +6,6 @@ import FormFieldArrayWrapper from '../../FormFields/FormFieldArrayWrapper';
 import FormFieldTextInput from '../../FormFields/FormFieldTextInput';
 import FormFieldImageSelect from '../../FormFields/FormFieldImageSelect';
 import FormFieldCheckboxGroup from '../../FormFields/FormFieldCheckboxGroup';
-import FormFieldSelectBox from '../../FormFields/FormFieldSelectBox';
 import {RecipeServings} from './RecipeFields/Servings';
 import {IngredientList} from './RecipeFields/IngredientList';
 import * as recipeData from '../../../constants/recipeData';
@@ -25,50 +24,32 @@ export class RecipeEditor extends React.Component {
   }
 
   render () {
-    console.log(this.props.atom.recipe.tags.cuisine);
-    
     return (
       <ManagedForm data={this.props.atom} updateData={this.props.onUpdate}>
-        
-        <h3 className="form__subheading">Tags</h3>
-        <div className="form__flex-container">
-          <div className="form__flex-container__item">
-            <ManagedField fieldLocation="data.recipe.tags.cuisine" name="Cuisine">
-              <FormFieldCheckboxGroup checkValues={recipeData.cuisineList}/>
-            </ManagedField>
-          </div>
-          <div className="form__flex-container__item">
-            <ManagedField fieldLocation="data.recipe.tags.category" name="Category">
-              <FormFieldCheckboxGroup checkValues={recipeData.categoryList}/>
-            </ManagedField>
-          </div>
-        </div>
-        <div className="form__flex-container">
-          <div className="form__flex-container__item">
-            <ManagedField fieldLocation="data.recipe.tags.celebration" name="Celebration">
-              <FormFieldCheckboxGroup checkValues={recipeData.celebrationList}/>
-            </ManagedField>
-          </div>
-          <div className="form__flex-container__item">
-            <ManagedField fieldLocation="data.recipe.tags.dietary" name="Dietary info">
-              <FormFieldCheckboxGroup checkValues={recipeData.dietaryList}/>
-            </ManagedField>
-          </div>
-        </div>
 
-        <h3 className="form__subheading">Time</h3>
-        <div className="form__flex-container">
-          <div className="form__flex-container__item">
-            <ManagedField fieldLocation="data.recipe.time.preparation" name="Preparation Time (mins)">
-              <FormFieldNumericInput/>
-            </ManagedField>
-          </div>
-          <div className="form__flex-container__item">
-            <ManagedField fieldLocation="data.recipe.time.cooking" name="Cooking Time (mins)">
-              <FormFieldNumericInput/>
-            </ManagedField>
-          </div>
-        </div>
+        <ManagedField fieldLocation="data.recipe.tags.cuisine" name="Cuisine">
+          <FormFieldCheckboxGroup checkValues={recipeData.cuisineList}/>
+        </ManagedField>
+        <ManagedField fieldLocation="data.recipe.tags.category" name="Category">
+          <FormFieldCheckboxGroup checkValues={recipeData.categoryList}/>
+        </ManagedField>
+
+        <ManagedField fieldLocation="data.recipe.tags.celebration" name="Celebration">
+          <FormFieldCheckboxGroup checkValues={recipeData.celebrationList}/>
+        </ManagedField>
+
+        <ManagedField fieldLocation="data.recipe.tags.dietary" name="Dietary info">
+          <FormFieldCheckboxGroup checkValues={recipeData.dietaryList}/>
+        </ManagedField>
+
+        <ManagedField fieldLocation="data.recipe.time.preparation" name="Preparation Time (mins)">
+          <FormFieldNumericInput/>
+        </ManagedField>
+
+        <ManagedField fieldLocation="data.recipe.time.cooking" name="Cooking Time (mins)">
+          <FormFieldNumericInput/>
+        </ManagedField>
+
         <ManagedField fieldLocation="data.recipe.serves" name="Serving Information">
           <RecipeServings />
         </ManagedField>
