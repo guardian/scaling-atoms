b2cdd5b41070eb830090ff8f17f5e0d76751137c
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-03-27 17:30:51 +0100

    add recipe tag fields to editor form

--- public/js/components/AtomEdit/CustomEditors/RecipeEditor.js
+++ public/js/components/AtomEdit/CustomEditors/RecipeEditor.js
@@ -5,8 +5,11 @@ import FormFieldNumericInput from '../../FormFields/FormFieldNumericInput';
 import FormFieldArrayWrapper from '../../FormFields/FormFieldArrayWrapper';
 import FormFieldTextInput from '../../FormFields/FormFieldTextInput';
 import FormFieldImageSelect from '../../FormFields/FormFieldImageSelect';
+import FormFieldCheckboxGroup from '../../FormFields/FormFieldCheckboxGroup';
+import FormFieldSelectBox from '../../FormFields/FormFieldSelectBox';
 import {RecipeServings} from './RecipeFields/Servings';
 import {IngredientList} from './RecipeFields/IngredientList';
+import * as recipeData from '../../../constants/recipeData';
 
 export class RecipeEditor extends React.Component {
 
@@ -22,9 +25,37 @@ export class RecipeEditor extends React.Component {
   }
 
   render () {
-
+    console.log(this.props.atom.recipe.tags.cuisine);
+    
     return (
       <ManagedForm data={this.props.atom} updateData={this.props.onUpdate}>
+        
+        <h3 className="form__subheading">Tags</h3>
+        <div className="form__flex-container">
+          <div className="form__flex-container__item">
+            <ManagedField fieldLocation="data.recipe.tags.cuisine" name="Cuisine">
+              <FormFieldCheckboxGroup checkValues={recipeData.cuisineList}/>
+            </ManagedField>
+          </div>
+          <div className="form__flex-container__item">
+            <ManagedField fieldLocation="data.recipe.tags.category" name="Category">
+              <FormFieldCheckboxGroup checkValues={recipeData.categoryList}/>
+            </ManagedField>
+          </div>
+        </div>
+        <div className="form__flex-container">
+          <div className="form__flex-container__item">
+            <ManagedField fieldLocation="data.recipe.tags.celebration" name="Celebration">
+              <FormFieldCheckboxGroup checkValues={recipeData.celebrationList}/>
+            </ManagedField>
+          </div>
+          <div className="form__flex-container__item">
+            <ManagedField fieldLocation="data.recipe.tags.dietary" name="Dietary info">
+              <FormFieldCheckboxGroup checkValues={recipeData.dietaryList}/>
+            </ManagedField>
+          </div>
+        </div>
+
         <h3 className="form__subheading">Time</h3>
         <div className="form__flex-container">
           <div className="form__flex-container__item">
---
+++ public/js/constants/recipeData.js
@@ -0,0 +1,76 @@
+export const cuisineList = [
+  'African',
+  'American',
+  'Asian',
+  'BBQ',
+  'British',
+  'Caribbean',
+  'Chinese',
+  'French',
+  'Greek',
+  'Indian',
+  'Irish',
+  'Italian',
+  'Japanese',
+  'Korean',
+  'Lebanese',
+  'Mediterranean',
+  'Mexican',
+  'Moroccan',
+  'Nordic',
+  'North African',
+  'Persian',
+  'Polish',
+  'Portuguese',
+  'South American',
+  'Spanish',
+  'Thai',
+  'Turkish',
+  'Vietnamese'
+];
+
+export const categoryList = [
+  'Baking',
+  'Barbecue',
+  'Breakfast',
+  'Budget',
+  'Canapes',
+  'Dessert',
+  'Dinner',
+  'Dinner party',
+  'Drinks and cocktails',
+  'Healthy eating',
+  'Lunch',
+  'Main course',
+  'Picnic',
+  'Salads',
+  'Sandwich',
+  'Sides',
+  'Soup',
+  'Snacks',
+  'Starters'
+];
+
+export const celebrationList = [
+  'Baisakhi',
+  'Christmas',
+  'Diwali',
+  'Easter',
+  'Eid',
+  'Halloween',
+  'Hanukkah',
+  'Passover',
+  'Thanksgiving'
+];
+
+export const dietaryList = [
+  'Low sugar',
+  'Low fat',
+  'High fibre',
+  'Nut free',
+  'Gluten free',
+  'Dairy free',
+  'Egg free',
+  'Vegetarian',
+  'Vegan'
+];
