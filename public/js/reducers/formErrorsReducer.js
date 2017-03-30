ab72e11c6908dced3b8dc3418c44399b7490f35b
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-03-30 11:41:34 +0100

    make the linter happy

--- public/js/reducers/formErrorsReducer.js
+++ public/js/reducers/formErrorsReducer.js
@@ -1,12 +1,13 @@
 export default function formErrors(state = {}, action) {
+  
+  const formName = Object.keys(action.error)[0];
+  const newFormErrors = action.error[formName];
+  const currentFormErrors = state[formName] || {};
+  const updatedFormErrors = Object.assign({}, currentFormErrors, newFormErrors);
+  const updatedForm = {[formName]: updatedFormErrors};
 
   switch (action.type) {
     case 'FORM_ERRORS_UPDATE_REQUEST':
-      const formName = Object.keys(action.error)[0];
-      const newFormErrors = action.error[formName];
-      const currentFormErrors = state[formName] || {};
-      const updatedFormErrors = Object.assign({}, currentFormErrors, newFormErrors);
-      const updatedForm = {[formName]: updatedFormErrors};
       return Object.assign({}, state, updatedForm);
 
     default:
