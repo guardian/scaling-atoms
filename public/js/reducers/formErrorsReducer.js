6149dd3d84901c7ac8d905d5035971afaee0f927
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-03-30 11:40:30 +0100

    use const FOR STRENGTH

--- public/js/reducers/formErrorsReducer.js
+++ public/js/reducers/formErrorsReducer.js
@@ -2,11 +2,11 @@ export default function formErrors(state = {}, action) {
 
   switch (action.type) {
     case 'FORM_ERRORS_UPDATE_REQUEST':
-      var formName = Object.keys(action.error)[0],
-          newFormErrors = action.error[formName],
-          currentFormErrors = state[formName] || {},
-          updatedFormErrors = Object.assign({}, currentFormErrors, newFormErrors),
-          updatedForm = {[formName]: updatedFormErrors};
+      const formName = Object.keys(action.error)[0];
+      const newFormErrors = action.error[formName];
+      const currentFormErrors = state[formName] || {};
+      const updatedFormErrors = Object.assign({}, currentFormErrors, newFormErrors);
+      const updatedForm = {[formName]: updatedFormErrors};
       return Object.assign({}, state, updatedForm);
 
     default:
