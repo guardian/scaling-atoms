2828523b88fd170d7f1a9650b06e35377842be1c
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-03-30 10:15:38 +0100

    move var declarations inside case

--- public/js/reducers/formErrorsReducer.js
+++ public/js/reducers/formErrorsReducer.js
@@ -1,13 +1,12 @@
 export default function formErrors(state = {}, action) {
 
-  var formName, newFormErrors, currentFormErrors, updatedFormErrors, updatedForm;
   switch (action.type) {
     case 'FORM_ERRORS_UPDATE_REQUEST':
-      formName = Object.keys(action.error)[0];
-      newFormErrors = action.error[formName];
-      currentFormErrors = state[formName] || {};
-      updatedFormErrors = Object.assign({}, currentFormErrors, newFormErrors);
-      updatedForm = {[formName]: updatedFormErrors};
+      var formName = Object.keys(action.error)[0],
+          newFormErrors = action.error[formName],
+          currentFormErrors = state[formName] || {},
+          updatedFormErrors = Object.assign({}, currentFormErrors, newFormErrors),
+          updatedForm = {[formName]: updatedFormErrors};
       return Object.assign({}, state, updatedForm);
 
     default:
